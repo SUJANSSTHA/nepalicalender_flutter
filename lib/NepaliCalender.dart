077804a4769ca4515d9f430b6f073c901d649ec6
SUJAN PRADHAN <123sujanpr@gmail.com>
2024-09-06 23:11:49 +0545

    first commit

---
+++ lib/NepaliCalender.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bs_ad_calendar/flutter_bs_ad_calendar.dart';
+
+class NepaliCalender extends StatefulWidget {
+  const NepaliCalender({super.key});
+
+  @override
+  State<NepaliCalender> createState() => _NepaliCalenderState();
+}
+
+class _NepaliCalenderState extends State<NepaliCalender> {
+  DateTime? selectedDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nepali Calendar"),
+      ),
+      body: FlutterBSADCalendar(
+        initialDate: DateTime.now(), // Today's date
+        firstDate: DateTime(1970),  // First allowed date
+        lastDate: DateTime(2024, 12, 31), // Updated last date to be after the initial date
+        onMonthChanged: (date, event) {
+          setState(() {
+            selectedDate = date;
+          });
+        },
+        onDateSelected: (date, events) {
+          setState(() {
+            selectedDate = date;
+          });
+        },
+      ),
+    );
+  }
+}
