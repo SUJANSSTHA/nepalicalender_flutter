d097e65c6d56802bc2ff8acae2f97f682f29609f
SUJAN PRADHAN <123sujanpr@gmail.com>
2024-09-08 08:30:08 +0545

    import 'package:nepalicalender/NepaliCalender.dart';

--- lib/NepaliCalender.dart
+++ lib/NepaliCalender.dart
@@ -15,16 +15,24 @@ class _NepaliCalenderState extends State<NepaliCalender> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Nepali Calendar",style: TextStyle(fontSize: 19.0,fontWeight: FontWeight.bold,fontStyle:FontStyle.italic,),),
+        title: const Text(
+          "Nepali Calendar",
+          style: TextStyle(
+            fontSize: 19.0,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
         backgroundColor: Colors.red,
         centerTitle: true,
-        leading: Icon(Icons.calendar_month),
+        leading: const Icon(Icons.calendar_month),
         // leading: Icon(Icons.NepaliCalender),
       ),
       body: FlutterBSADCalendar(
         initialDate: DateTime.now(), // Today's date
-        firstDate: DateTime(1970),  // First allowed date
-        lastDate: DateTime(2024, 12, 31), // Updated last date to be after the initial date
+        firstDate: DateTime(1970), // First allowed date
+        lastDate: DateTime(
+            2024, 12, 31), // Updated last date to be after the initial date
         onMonthChanged: (date, event) {
           setState(() {
             selectedDate = date;
